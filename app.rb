f17736180fa4554f6dd05c3df682844cecac543e
juharyimami <juharyimami2011@gmail.com>
2023-02-01 15:28:04 +0300

    basic UI added

--- app.rb
+++ app.rb
@@ -1,17 +1,3 @@
-# Create a app.rb file that will serve as your console app entry-point. It should have methods that do the following:
-#   List all books.
-#   List all people.
-#   Create a person (teacher or student, not a plain Person).
-#   Create a book.
-#   Create a rental.
-#   List all rentals for a given person id.
-#   In your main.rb define the entry point, this will be a method called main
-# that is invoked at the end of your file. This method should do the following:
-#   Present the user with a list of options to perform.
-#   Lets users choose an option.
-#   If needed, ask for parameters for the option.
-#   Have a way to quit the app.
-
 require_relative 'person'
 require_relative 'student'
 require_relative 'teacher'
