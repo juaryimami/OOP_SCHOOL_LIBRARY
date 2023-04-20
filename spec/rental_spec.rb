8b23b58eb7477b36f075245fad5e99fb62ab9464
aubinleyoung <aubinsimpeze01@gmail.com>
2023-04-20 12:07:41 +0200

    Add test for Rental class

---
+++ spec/rental_spec.rb
@@ -0,0 +1,20 @@
+# require 'rspec/autorun'
+require_relative '../classes/rental.rb'
+require_relative '../classes/book.rb'
+require_relative '../classes/person.rb'
+
+describe  Rental  do 
+it 'should initialize with a book, person, and date' do 
+  book = Book.new('The Catcher in the Rye', 'J.D. Salinger')
+  person = Person.new('John Doe')
+  date = '21/06/2022'
+
+  rental = Rental.new(book, person, date)
+
+  expect(rental.book).to eq(book)
+  expect(rental.person).to eq(person)
+  expect(rental.date).to eq(date)
+  expect(person.rentals).to include(rental)
+  expect(book.rentals).to include(rental)
+end
+end
